2d7a70fd98ff18b520a13ed9578204d094006e3a
kgryte <kgryte@gmail.com>
2016-01-28 15:57:42 -0800

    Split tests into numeric regimes and update Julia test runner

--- test/test.js
+++ test/test.js
@@ -14,7 +14,11 @@ var expm1 = require( './../lib' );
 
 // FIXTURES //
 
-var small = require( './fixtures/small.json' );
+var mediumNegative = require( './fixtures/medium_negative.json' );
+var mediumPositive = require( './fixtures/medium_positive.json' );
+var smallNegative = require( './fixtures/small_negative.json' );
+var smallPositive = require( './fixtures/small_positive.json' );
+var tiny = require( './fixtures/tiny.json' );
 
 
 // TESTS //
@@ -44,17 +48,102 @@ tape( 'the function agrees with `exp(x) - 1` for most `x`', function test( t ) {
 	t.end();
 });
 
-tape( 'the function accurately computes `exp(x) - 1` for very small `x`', function test( t ) {
+tape( 'the function accurately computes `exp(x) - 1` for negative medium numbers', function test( t ) {
+	var expected;
 	var delta;
 	var tol;
+	var x;
 	var v;
 	var i;
 
-	for ( i = 0; i < small.data.length; i++ ) {
-		v = expm1( small.data[ i ] );
-		delta = abs( v - small.expected[ i ] );
-		tol = 1e-12 * Math.max( 1, abs( v ), abs( small.expected[ i ] ) );
-		t.ok( delta <= tol, 'within tolerance. x: ' + small.data[ i ] + '. Value: ' + v + '. Expected: ' + small.expected[ i ] + '. Tolerance: ' + tol + '.' );
+	x = mediumNegative.x;
+	expected = mediumNegative.expected;
+
+	for ( i = 0; i < x.length; i++ ) {
+		v = expm1( x[ i ] );
+		delta = abs( v - expected[ i ] );
+		tol = 1e-12 * Math.max( 1, abs( v ), abs( expected[ i ] ) );
+		t.ok( delta <= tol, 'within tolerance. x: ' + x[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
+	}
+	t.end();
+});
+
+tape( 'the function accurately computes `exp(x) - 1` for positive medium numbers', function test( t ) {
+	var expected;
+	var delta;
+	var tol;
+	var x;
+	var v;
+	var i;
+
+	x = mediumPositive.x;
+	expected = mediumPositive.expected;
+
+	for ( i = 0; i < x.length; i++ ) {
+		v = expm1( x[ i ] );
+		delta = abs( v - expected[ i ] );
+		tol = 1e-12 * Math.max( 1, abs( v ), abs( expected[ i ] ) );
+		t.ok( delta <= tol, 'within tolerance. x: ' + x[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
+	}
+	t.end();
+});
+
+tape( 'the function accurately computes `exp(x) - 1` for negative small numbers', function test( t ) {
+	var expected;
+	var delta;
+	var tol;
+	var x;
+	var v;
+	var i;
+
+	x = smallNegative.x;
+	expected = smallNegative.expected;
+
+	for ( i = 0; i < x.length; i++ ) {
+		v = expm1( x[ i ] );
+		delta = abs( v - expected[ i ] );
+		tol = 1e-12 * Math.max( 1, abs( v ), abs( expected[ i ] ) );
+		t.ok( delta <= tol, 'within tolerance. x: ' + x[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
+	}
+	t.end();
+});
+
+tape( 'the function accurately computes `exp(x) - 1` for positive small numbers', function test( t ) {
+	var expected;
+	var delta;
+	var tol;
+	var x;
+	var v;
+	var i;
+
+	x = smallPositive.x;
+	expected = smallPositive.expected;
+
+	for ( i = 0; i < x.length; i++ ) {
+		v = expm1( x[ i ] );
+		delta = abs( v - expected[ i ] );
+		tol = 1e-12 * Math.max( 1, abs( v ), abs( expected[ i ] ) );
+		t.ok( delta <= tol, 'within tolerance. x: ' + x[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
+	}
+	t.end();
+});
+
+tape( 'the function accurately computes `exp(x) - 1` for very small `x`', function test( t ) {
+	var expected;
+	var delta;
+	var tol;
+	var x;
+	var v;
+	var i;
+
+	x = tiny.x;
+	expected = tiny.expected;
+
+	for ( i = 0; i < x.length; i++ ) {
+		v = expm1( x[ i ] );
+		delta = abs( v - expected[ i ] );
+		tol = 1e-12 * Math.max( 1, abs( v ), abs( expected[ i ] ) );
+		t.ok( delta <= tol, 'within tolerance. x: ' + x[ i ] + '. Value: ' + v + '. Expected: ' + expected[ i ] + '. Tolerance: ' + tol + '.' );
 	}
 	t.end();
 });
@@ -66,7 +155,7 @@ tape( 'the function returns `+infinity` for very large `x`', function test( t ) 
 	t.end();
 });
 
-tape( 'the function returns `-1` for negative, large `x`', function test( t ) {
+tape( 'the function returns `-1` for negative large `x`', function test( t ) {
 	t.equal( expm1( -800 ), -1, 'equals -1' );
 	t.equal( expm1( -900 ), -1, 'equals -1' );
 	t.equal( expm1( -1000 ), -1, 'equals -1' );
